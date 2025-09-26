667980402c93e6cd155a9885f9df33e0ce2bcd52
kshitjkatore <kshitijkatore03@gmail.com>
2025-09-26 10:40:24 +0530

    Kettle Boiling Story Project

---
+++ 01_Revised-Python/02_Conditionals/01_Kettle_Boiling_Story_Project.py
@@ -0,0 +1,8 @@
+## You're creating a notification system for a smart kettel. It should remind the user only when the kettel has fininshed boiling. Task:
+## A variable kettle_boiled = True
+##if boiled show: "Kettel done! Time to make chai !"
+
+kettle_boiled = True
+
+if kettle_boiled:
+    print("Kettle Done! Time make chai.")
