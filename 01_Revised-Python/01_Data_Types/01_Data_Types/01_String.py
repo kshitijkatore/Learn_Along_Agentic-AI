618bad8cc503e73d4769549a87355520d12cd6fa
kshitjkatore <kshitijkatore03@gmail.com>
2025-09-26 10:49:28 +0530

    Building Snack System

---
+++ 01_Revised-Python/01_Data_Types/01_Data_Types/01_String.py
@@ -0,0 +1,9 @@
+chai_type = 'Ginger chai'
+customer_name = "XItij"
+
+print(f"ORder for {customer_name} : {chai_type} please!")
+
+chai_description ="Aromatic and Bold"
+print(f"First word: {chai_description[0:8]}")
+
+# Encoding Decoding
---
+++ 01_Revised-Python/01_Data_Types/01_Data_Types/01_Tuples.py
@@ -0,0 +1,8 @@
+masala_spices = ("Cardamom", "cloves", "cinnamon")
+
+(spice1, spice2, spice3) = masala_spices
+print(f"Main masala spices : {spice1}, {spice2}, {spice3}")
+
+# membrship testing
+
+print(f"IS cinnamon in masala spices ? {'cinnamon' in masala_spices}")
