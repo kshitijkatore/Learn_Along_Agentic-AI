c74d7b5cb45a47f9254bd417891bbd4db3798bbf
kshitjkatore <kshitijkatore03@gmail.com>
2025-09-28 09:39:18 +0530

    Calling multiple functions

---
+++ 01_Revised-Python/05_Functions/02_complex_task.py
@@ -0,0 +1,17 @@
+def fetch_sales():
+    print("Featching sales data")
+
+
+def filter_valid_sales():
+    print("Filtering valid sales data.")
+
+def summarize_data():
+        print("Summarizing sales data.")
+
+def generatee_report():
+     fetch_sales()
+     filter_valid_sales()
+     summarize_data()
+     print("Report is ready!!")
+
+generatee_report()
