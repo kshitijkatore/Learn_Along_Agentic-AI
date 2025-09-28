fbce4b0642d66ef37b9cc9a7322cf2279283dc2c
kshitjkatore <kshitijkatore03@gmail.com>
2025-09-28 10:00:09 +0530

    Exploring Hiding, Readability on functions

---
+++ 01_Revised-Python/05_Functions/03_hiding.py
@@ -0,0 +1,18 @@
+def get_input():
+    print("Getting user input")
+
+def valid_info():
+    print("Validating the user info")
+
+def save_to_db():
+    print("Saving to database")
+
+def register_user():
+    get_input()
+    valid_info()
+    save_to_db()
+    print("User registration complete.")
+
+register_user()
+
+
---
+++ 01_Revised-Python/05_Functions/04_readability.py
@@ -0,0 +1,5 @@
+def calculate_bill(cups, price_per_cup):
+    return cups * price_per_cup
+
+my_bill = calculate_bill(2, 50)
+print(my_bill)
